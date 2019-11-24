168ed7faed963ae1c2892607d3ee89619eaf7b31
Mohamed Elasfoury <mohamed.elasfoury@epfl.ch>
2019-11-24 12:29:30 +0100

    6 needs to be fixed

--- tetris.asm
+++ tetris.asm
@@ -65,17 +65,195 @@
 
 
   ;; TODO Insert your code here
-font_data:
-    .word 0xFC  ; 0
-    .word 0x60  ; 1
-    .word 0xDA  ; 2
-    .word 0xF2  ; 3
-    .word 0x66  ; 4
-    .word 0xB6  ; 5
-    .word 0xBE  ; 6
-    .word 0xE0  ; 7
-    .word 0xFE  ; 8
-    .word 0xF6  ; 9
+
+;BEGIN:main
+main:
+	addi sp, zero, LEDS
+	call clear_leds
+	add a0, zero, zero
+	add a1, zero, zero
+	addi a2,zero, FALLING
+	call set_gsa
+	call draw_gsa
+	ret
+;END:main
+
+
+
+; BEGIN:clear_leds
+clear_leds:
+	stw zero, LEDS (zero) ; 0 -> 3
+	stw zero, LEDS + 4 (zero) ; 4 -> 7
+	stw zero, LEDS + 8 (zero) ; 8 -> 11
+	ret
+; END:clear_leds
+
+
+; BEGIN:set_pixel
+set_pixel:
+	; Initialize registers
+	addi t0, zero, 0
+	addi t1, zero, 0
+	addi t2, zero, 0
+	addi t3, zero, 1
+
+	; a0 = x, a1 = y
+	srli t0, a0, 2 		; t0 = x/4 --> LED[0, 1 ou 2]
+	slli t0, t0, 2 		; t0 = LED + t0 * 4
+	slli t1, a0, 3		; t1 = 8 * a0
+	add t1, t1, a1		; t1 = t1 + a1
+	andi t1, t1, 0x1F	; t1 = t1 % 32
+	sll  t3, t3, t1
+	addi t2, t0, LEDS	; t2 = LED + t0
+	stw t3, 0 (t2)
+	ret
+; END:set_pixel
+
+; BEGIN:wait
+wait:
+	addi t0, zero, 1
+	slli t0, t0, 20
+	loop: 
+		beq t0, zero, done
+		addi t0, t0, -1
+		call loop
+	done:
+		ret
+; END:wait
+
+; BEGIN:in_gsa
+in_gsa:
+	add t0, zero, a0 ; x
+	add t1, zero, a1 ; y
+	cmpgei t2, t0, 12
+	bne t2, zero, retOne
+	cmpgei t2, t1, 8
+	bne t2, zero, retOne
+	cmplti t2, t0, 0
+	bne t2, zero, retOne
+	cmplti t2, t1, 0
+	bne t2, zero, retOne
+	addi v0, zero, 0
+	ret
+	retOne:
+		addi v0, zero, 1
+		ret 
+;END:in_gsa
+
+;BEGIN:get_gsa
+get_gsa:
+	add t0, zero, a0 ; x
+	add t1, zero, a1 ; y
+	slli t0, t0, 3 ; 8x
+	add t0, t0, t1 ; 8x + y
+	slli t0, t0, 2 ; (8x + y)* 4
+	ldw v0, GSA(t0)
+	ret
+;END:get_gsa
+
+;BEGIN:set_gsa
+set_gsa:
+	add t0, zero, a0 ; x
+	add t1, zero, a1 ; y
+	slli t0, t0, 3 ; 8x
+	add t0, t0, t1 ; 8x + y
+	slli t0, t0, 2 ; (8x + y)* 4
+	stw a2, GSA (t0)
+	ret
+;END:set_gsa
+;BEGIN:draw_gsa
+draw_gsa:
+	addi sp, sp, -12
+	stw ra, 0(sp)
+	stw s0, 4(sp)
+	stw s1, 8(sp)
+	
+	add s0, zero, zero ; s0 = x
+	add s1, zero, zero ; s1 = y
+	addi t5, zero, 12  ; t5 = 12 when x == 12 end
+	addi t6, zero, 8   ; t6 = 8 when y == 8 end 
+	
+	loopX:
+	beq s0, t5, end ; s0 == t5 end
+	add a0, zero, s0 ; 
+	loopY:
+	beq s1, t6, incX
+	add a1, zero, s1
+	call get_gsa 
+	beq v0, zero, dontSet
+	call set_pixel
+	dontSet:
+	addi s1, s1, 1
+	call loopY
+	incX:
+	add s1, zero, zero
+	addi s0, s0, 1
+	call loopX
+
+	
+	end:
+	ldw ra, 0(sp)
+	ldw s0, 4(sp)
+	ldw s1, 8(sp)
+	addi sp, sp, 12 
+	ret
+;END:draw_gsa
+
+
+; BEGIN:draw_tetromino
+draw_tetromino: ; need to take into account a0 now sets activates only tetremino
+	addi sp, sp, -4
+	stw s0, 0(sp)
+	
+	addi sp, sp, -4
+	stw s1, 0(sp)
+
+	addi sp, sp, -4
+	stw s2, 0(sp)
+
+	ldw s0, T_X(zero)
+	ldw s1, T_Y(zero)
+	ldw t2, T_orientation(zero)
+	ldw t3, T_type(zero)
+	slli t3, t3, 4 ; Type*16
+	slli t2, t2, 2; orientation * 4
+	add t2, t2, t3; Type*16+ orientation * 4
+	
+	ldw t3, DRAW_Ax(t2) ; gets address of shape from DRAW_Ax/DRAW_Ay
+	ldw t4, 0(t3) ; loads x offsets A1
+	ldw t5, 4(t3) ; A2
+	ldw t6, 8(t3) ; A3
+
+	ldw t7, DRAW_Ay(t2) ; loads y offsets
+	ldw t0, 0(t3) ; B1
+	ldw t1, 4(t3) ; B2
+	ldw s2, 8(t3) ; B3
+	
+	add a0, zero, s0
+	add a1, zero, s1
+	call set_pixel
+	
+	add t4, t4, s0
+	add t0, t0, s1
+	call set_pixel
+	
+	add t5, t5, s0
+	add t1, t1, s1
+	call set_pixel
+	
+	add t6, t6, s0
+	add s2, s2, s1
+	call set_pixel
+
+	ldw s2, 0(sp)
+	addi sp, sp, 4
+	
+	ldw s1, 0(sp)
+	addi sp, sp, 4
+	
+	ldw s0, 0(sp)
+	addi sp, sp, 4
+;END:draw_tetromino
 
 C_N_X:
   .word 0x00
@@ -248,14 +426,14 @@ L_N_Y:
   .word 0xFFFFFFFF
 
 L_E_X:
-  .word 0x00
-  .word 0x00
-  .word 0x01
+  .word 0x00 ; 0
+  .word 0x00 ; 0
+  .word 0x01 ; 1
 
 L_E_Y:
-  .word 0xFFFFFFFF
-  .word 0x01
-  .word 0x01
+  .word 0xFFFFFFFF ; -1
+  .word 0x01 ; 1
+  .word 0x01 ; 1
 
 L_So_X:
   .word 0xFFFFFFFF
@@ -278,26 +456,26 @@ L_W_Y:
   .word 0xFFFFFFFF
 
 DRAW_Ax:                        ; address of shape arrays, x axis
-    .word C_N_X
-    .word C_E_X
-    .word C_So_X
-    .word C_W_X
-    .word B_N_X
-    .word B_E_X
-    .word B_So_X
-    .word B_W_X
-    .word T_N_X
-    .word T_E_X
-    .word T_So_X
-    .word T_W_X
-    .word S_N_X
-    .word S_E_X
-    .word S_So_X
-    .word S_W_X
-    .word L_N_X
-    .word L_E_X
-    .word L_So_X
-    .word L_W_X
+    .word C_N_X ; 0 C start 
+    .word C_E_X ; 4
+    .word C_So_X ; 8
+    .word C_W_X ; 12
+    .word B_N_X ; 16 B start
+    .word B_E_X ; 20
+    .word B_So_X ; 24
+    .word B_W_X  ; 28
+    .word T_N_X ; 32 T start
+    .word T_E_X ; 36
+    .word T_So_X ; 40
+    .word T_W_X ; 44
+    .word S_N_X ; 48 S start
+    .word S_E_X ; 52
+    .word S_So_X ; 56
+    .word S_W_X ; 60
+    .word L_N_X ; 64 L start
+    .word L_E_X ; 68
+    .word L_So_X ; 72
+    .word L_W_X ; 76
 
 DRAW_Ay:                        ; address of shape arrays, y_axis
     .word C_N_Y
@@ -321,100 +499,6 @@ DRAW_Ay:                        ; address of shape arrays, y_axis
     .word L_So_Y
     .word L_W_Y
 
-
-;BEGIN:main
-main:
-	call clear_leds
-	addi a0, zero, 0
-	addi a1, zero, 0
-	call set_pixel
-;END:main
-
-
-
-; BEGIN:clear_leds
-clear_leds:
-	stw zero, LEDS (zero) ; 0 -> 3
-	stw zero, LEDS + 4 (zero) ; 4 -> 7
-	stw zero, LEDS + 8 (zero) ; 8 -> 11
-	ret
-; END:clear_leds
-
-
-; BEGIN:set_pixel
-set_pixel:
-	; Initialize registers
-	addi a0, zero, 0 ; this gets passed from outside
-	addi a1, zero, 0 ; this gets passed from outside
-	addi t0, zero, 0
-	addi t1, zero, 0
-	addi t2, zero, 0
-	addi t3, zero, 1
-
-	; a0 = x, a1 = y
-	srli t0, a0, 2 		; t0 = x/4 --> LED[0, 1 ou 2]
-	slli t0, t0, 2 		; t0 = LED + t0 * 4
-	slli t1, a0, 3		; t1 = 8 * a0
-	add t1, t1, a1		; t1 = t1 + a1
-	andi t1, t1, 0x1F	; t1 = t1 % 32
-	sll  t3, t3, t1
-	addi t2, t0, LEDS	; t2 = LED + t0
-	stw t3, 0 (t2)
-	ret
-; END:set_pixel
-
-; BEGIN:wait
-wait:
-	addi t0, zero, 1
-	slli t0, t0, 20
-	loop: 
-		beq t0, zero, done
-		addi t0, t0, -1
-		call loop
-	done:
-		ret
-; END:wait
-
-; BEGIN:in_gsa
-in_gsa:
-	add t0, zero, a0 ; x
-	add t1, zero, a1 ; y
-	cmpgei t2, t0, 12
-	bne t2, zero, retOne
-	cmpgei t2, t1, 8
-	bne t2, zero, retOne
-	cmplti t2, t0, 0
-	bne t2, zero, retOne
-	cmplti t2, t1, 0
-	bne t2, zero, retOne
-	addi v0, zero, 0
-	ret
-	retOne:
-		addi v0, zero, 1
-		ret 
-;END:in_gsa
-
-;BEGIN:get_gsa
-get_gsa:
-	add t0, zero, a0 ; x
-	add t1, zero, a1 ; y
-	slli t0, t0, 3 ; 8x
-	add t0, t0, t1 ; 8x + y
-	slli t0, t0, 2 ; (8x + y)* 4
-	ldw v0, GSA(t0)
-	ret
-;END:get_gsa
-
-;BEGIN:set_gsa
-set_gsa:
-	add t0, zero, a0 ; x
-	add t1, zero, a1 ; y
-	slli t0, t0, 3 ; 8x
-	add t0, t0, t1 ; 8x + y
-	slli t0, t0, 2 ; (8x + y)* 4
-	stw a2, GSA (t0)
-	ret
-;END:set_gsa
 
 
 
